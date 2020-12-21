e5288ac23bc5c0ebef80cacea0f54f957eb0adbf
cbarco <cbarco@capmotion.net>
2020-12-21 17:08:03 -0500

    Initial commit

---
+++ src/main/java/net/capmotion/datapipe/DataPipeApplication.java
@@ -0,0 +1,13 @@
+package net.capmotion.datapipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/capmotion/datapipe/DataPipeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.capmotion.datapipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
